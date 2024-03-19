fe9832298ed8c1dc4a323ca85214cd6046df57cd
AiDeeN A <aidencastillo41@gmail.com>
2024-03-19 12:15:36 -0600

    new wde2

---
+++ projects/wde2/config/app.php
@@ -0,0 +1,4 @@
+<?php
+
+const APP_URL = 'http://localhost/aidcascs40/projects/wde2/public';
+const SENDER_EMAIL_ADDRESS = 'aidencastillo41@gmail.com';
---
+++ projects/wde2/config/database.php
@@ -0,0 +1,6 @@
+<?php
+
+const DB_HOST = 'localhost:3307';
+const DB_NAME = 'auth';
+const DB_USER = 'root';
+const DB_PASSWORD = '';
